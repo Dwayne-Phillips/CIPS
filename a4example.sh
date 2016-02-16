0f0587616864b9d130f683fcd7c2e9147d7424b4
Dwayne Phillips <d.phillips@computer.org>
2016-02-16 09:23:01 -0500

    First example of a shell script to simplify experiments

---
+++ a4example.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+~/cips/himage a4.tif a4hist.tif 400 400
+~/cips/side a4.tif a4hist.tif a4side.tif Side-by-side
+
+~/cips/histeq a4.tif a4eq.tif
+~/cips/himage a4eq.tif a4eqhist.tif 400 400
+~/cips/side a4eq.tif a4eqhist.tif a4eqside.tif Side-by-side
+
+~/cips/side a4side.tif a4eqside.tif a4final.tif Top-to-bottom
+
+rm a4eq.tif
+rm a4eqhist.tif
+rm a4eqside.tif
+rm a4hist.tif
+rm a4side.tif
+
