681ce50058a7019a4a4a6d7141d2f81d8ca7feef
Dwayne-Phillips <d.phillips@computer.org>
2020-11-26 08:46:26 -0500

    Validated November 2020

--- hello.c
+++ hello.c
@@ -1,6 +1,7 @@
 
 
 
+
 #include <stdio.h>
 int hello2();
 int hello3();
@@ -8,13 +9,13 @@ int hello3();
 int main()
 {
    char buffer[80];
-   printf("\nhello out there"); 
-   
+   printf("\nhello out there");
+
    printf("\nEnter your name:");
    gets(buffer);
-   printf("\n\nyou entered>>%s<<\n", buffer);  
+   printf("\n\nyou entered>>%s<<\n", buffer);
 
    hello2();
    hello3();
    return(0);
-}  
+}
